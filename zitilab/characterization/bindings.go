8881646cf578366ab4bff9b26d1c988690f26c3f
Michael Quigley <michael@quigley.com>
2020-01-09 10:25:27 -0500

    What gets created, must be destroyed.

--- zitilab/characterization/bindings.go
+++ zitilab/characterization/bindings.go
@@ -21,6 +21,7 @@ import (
 	"github.com/netfoundry/fablab/kernel/model"
 	semaphore0 "github.com/netfoundry/fablab/kernel/runlevel/0_infrastructure/semaphore"
 	terraform0 "github.com/netfoundry/fablab/kernel/runlevel/0_infrastructure/terraform"
+	terraform6 "github.com/netfoundry/fablab/kernel/runlevel/6_disposal/terraform"
 	"time"
 )
 
@@ -32,6 +33,9 @@ func (f *bindingsFactory) Build(m *model.Model) error {
 	if err := f.buildInfrastructure(m); err != nil {
 		return fmt.Errorf("error building infrastructure bindings (%w)", err)
 	}
+	if err := f.buildDisposal(m); err != nil {
+		return fmt.Errorf("error building disposal bindings (%w)", err)
+	}
 	return nil
 }
 
@@ -39,6 +43,13 @@ func (f *bindingsFactory) buildInfrastructure(m *model.Model) error {
 	m.Infrastructure = model.InfrastructureBinders{
 		func(m *model.Model) model.InfrastructureStage { return terraform0.Express() },
 		func(m *model.Model) model.InfrastructureStage { return semaphore0.Restart(90 * time.Second) },
+	}
+	return nil
+}
+
+func (f *bindingsFactory) buildDisposal(m *model.Model) error {
+	m.Disposal = model.DisposalBinders{
+		func(m *model.Model) model.DisposalStage { return terraform6.Dispose() },
 	}
 	return nil
 }
