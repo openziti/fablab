231ee8869223636a2222f5a5e12ecc8b4665d03b
Michael Quigley <michael@quigley.com>
2021-01-12 11:11:54 -0500

    Use pfxlog color.

--- kernel/fablib/ssh.go
+++ kernel/fablib/ssh.go
@@ -38,7 +38,7 @@ import (
 var SshCommand string
 
 func LaunchService(factory SshConfigFactory, name, cfg string) error {
-	serviceCmd := fmt.Sprintf("nohup /home/%s/fablab/bin/%s --log-formatter pfxlog run /home/%s/fablab/cfg/%s > logs/%s.log 2>&1 &", factory.User(), name, factory.User(), cfg, name)
+	serviceCmd := fmt.Sprintf("export PFXLOG_USE_COLOR=true; nohup /home/%s/fablab/bin/%s --log-formatter pfxlog run /home/%s/fablab/cfg/%s > logs/%s.log 2>&1 &", factory.User(), name, factory.User(), cfg, name)
 	if value, err := RemoteExec(factory, serviceCmd); err == nil {
 		if len(value) > 0 {
 			logrus.Infof("output [%s]", strings.Trim(value, " \t\r\n"))
