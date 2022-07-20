0dbba087f73e9ba664891b0b83794cf17c663ab5
Cam Otts <otts.cameron@gmail.com>
2022-07-20 14:04:24 -0500

    added another distribution helper

--- kernel/lib/runlevel/3_distribution/distdata.go
+++ kernel/lib/runlevel/3_distribution/distdata.go
@@ -2,11 +2,54 @@ package distribution
 
 import (
 	"os"
+	"strings"
 
 	"github.com/openziti/fablab/kernel/lib"
 	"github.com/openziti/fablab/kernel/model"
 	"github.com/sirupsen/logrus"
 )
+
+func DistributeDataWithReplaceCallbacks(hostSpec, data, dest string, callbacks map[string]func(*model.Host) string) model.DistributionStage {
+	return &distDataWithReplaceCallbacks{
+		hostSpec:  hostSpec,
+		data:      data,
+		dest:      dest,
+		callbacks: callbacks,
+	}
+}
+
+func (df *distDataWithReplaceCallbacks) Distribute(run model.Run) error {
+	return run.GetModel().ForEachHost(df.hostSpec, 25, func(host *model.Host) error {
+		ssh := lib.NewSshConfigFactory(host)
+
+		dataRaw := df.data
+
+		for k, v := range df.callbacks {
+			dataRaw = strings.ReplaceAll(dataRaw, k, v(host))
+		}
+
+		if err := lib.SendData(ssh, []byte(dataRaw), df.dest); err != nil {
+			logrus.Errorf("[%s] unable to send data => %s", host.PublicIp, df.dest)
+			return err
+		}
+
+		if err := lib.Chmod(ssh, df.dest, os.FileMode(0644)); err != nil {
+			logrus.Errorf("[%s] unable to send data => %s", host.PublicIp, df.dest)
+			return err
+		}
+
+		logrus.Infof("[%s] data => %s", host.PublicIp, df.dest)
+
+		return nil
+	})
+}
+
+type distDataWithReplaceCallbacks struct {
+	hostSpec  string
+	data      string
+	dest      string
+	callbacks map[string]func(*model.Host) string
+}
 
 func DistributeData(hostSpec string, data []byte, dest string) model.DistributionStage {
 	return &distData{
