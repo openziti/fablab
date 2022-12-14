c06bd6a071736827675d249690c925f3f5ca45ba
Pete DiBona <pete.dibona@netfoundry.io>
2022-12-14 16:15:13 -0600

    Run Overlay first due to some sort of DNS-related timing issue.

--- kernel/lib/runlevel/5_operation/iperf.go
+++ kernel/lib/runlevel/5_operation/iperf.go
@@ -50,7 +50,7 @@ func (i *iperf) Operate(run model.Run) error {
 		sshClientFactory := lib.NewSshConfigFactory(host)
 
 		if i.overlay == false {
-			iperfCmdU := fmt.Sprintf("iperf3 -c %s -p 7001 -t %d -P 1 -b 5M --json", serverHost.PublicIp, i.seconds)
+			iperfCmdU := fmt.Sprintf("iperf3 -c %s -p 7001 -t %d -P 1 -b 640M --json", serverHost.PublicIp, i.seconds)
 			output, err := lib.RemoteExec(sshClientFactory, iperfCmdU)
 			if err == nil {
 				logrus.Debugf("output = [%s]", output)
@@ -67,7 +67,7 @@ func (i *iperf) Operate(run model.Run) error {
 				return fmt.Errorf("iperf3 client failure [%s] (%w)", output, err)
 			}
 		} else {
-			iperfCmdO := fmt.Sprintf("iperf3 -c %s -p 7001 -t %d -P 1 -b 5M --json", "iperf.service", i.seconds)
+			iperfCmdO := fmt.Sprintf("iperf3 -c %s -p 7001 -t %d -P 1 -b 640M --json", "iperf.service", i.seconds)
 			output, err := lib.RemoteExec(sshClientFactory, iperfCmdO)
 			if err == nil {
 				logrus.Debugf("output = [%s]", output)
