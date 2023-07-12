c4a50dcd835fac831aa98fcdef043efc049397d5
Pete DiBona <pete.dibona@netfoundry.io>
2023-07-12 16:07:38 -0500

    Switched to proper AMI for monitoring
    Signed-off-by: Pete DiBona <pete.dibona@netfoundry.io>

--- resources/terraform/main.tf
+++ resources/terraform/main.tf
@@ -31,12 +31,12 @@ variable "amis" {
 	eu-west-3		= "ami-0f7cd40eac2214b37"
 	me-south-1		= "ami-0eddb8cfbd6a5f657"
 	sa-east-1		= "ami-054a31f1b3bf90920"
-	us-east-1		= "ami-02de399c321d1e000"
+	us-east-1		= "ami-0464ed28453549213"
 	us-east-2		= "ami-00399ec92321828f5"
 	us-gov-east-1	= "ami-0dec4096f1af85e9b"
 	us-gov-west-1	= "ami-0c39aacd1cc8a1ccf"
 	us-west-1		= "ami-0d382e80be7ffdae5"
-	us-west-2		= "ami-0c1c6ba1d67974a2f"
+	us-west-2		= "ami-0329ee6c35395b766"
   }
 }
 
